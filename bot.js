4a8c1755efe582fdb699f4076a8b2d222579a50e
Siimon13 <chen.simon89@gmail.com>
2017-01-07 17:07:18 -0500

    stuff

--- bot.js
+++ bot.js
@@ -32,43 +32,44 @@ var findOrCreateSession = function (fbid) {
 }
 
 var read = function (sender, message, reply) {
-	if (message === 'hello') {
-		// Let's reply back hello
-		message = 'Hello yourself! I am a chat bot. You can say "show me pics of corgis"'
-		reply(sender, message)
-	} else {
-		// Let's find the user
-		var sessionId = findOrCreateSession(sender)
-		// Let's forward the message to the Wit.ai bot engine
-		// This will run all actions until there are no more actions left to do
-		wit.runActions(
-			sessionId, // the user's current session by id
-			message,  // the user's message
-			sessions[sessionId].context, // the user's session state
-			function (error, context) { // callback
-			if (error) {
-				console.log('oops!', error)
-			} else {
-				// Wit.ai ran all the actions
-				// Now it needs more messages
-				console.log('Waiting for further messages')
+    console.log(message);
+    if (message === 'hello') {
+	// Let's reply back hello
+	message = 'Hello yourself! I am a chat bot. You can say "show me pics of corgis"'
+	reply(sender, message)
+    } else {
+	// Let's find the user
+	var sessionId = findOrCreateSession(sender)
+	// Let's forward the message to the Wit.ai bot engine
+	// This will run all actions until there are no more actions left to do
+	wit.runActions(
+	    sessionId, // the user's current session by id
+	    message,  // the user's message
+	    sessions[sessionId].context, // the user's session state
+	    function (error, context) { // callback
+		if (error) {
+		    console.log('oops!', error)
+		} else {
+		    // Wit.ai ran all the actions
+		    // Now it needs more messages
+		    console.log('Waiting for further messages')
 
-				// Based on the session state, you might want to reset the session
-				// Example:
-				// if (context['done']) {
-				// 	delete sessions[sessionId]
-				// }
+		    // Based on the session state, you might want to reset the session
+		    // Example:
+		    // if (context['done']) {
+		    // 	delete sessions[sessionId]
+		    // }
 
-				// Updating the user's current session state
-				sessions[sessionId].context = context
-			}
-		})
-	}
+		    // Updating the user's current session state
+		    sessions[sessionId].context = context
+		}
+	    })
+    }
 }
 
 
 
 module.exports = {
-	findOrCreateSession: findOrCreateSession,
+    findOrCreateSession: findOrCreateSession,
 	read: read,
 }
